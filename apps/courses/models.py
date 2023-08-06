8dcafaef48da295905c7b93a17b6dd2ccdf42a57
Great <great.goodwin.92@gmail.com>
2023-08-06 14:08:10 +0300

    созданы модели course, lesson

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название курса")
+    description = models.TextField(verbose_name="Описание курса")
+    preview = models.ImageField(upload_to='course/', verbose_name='Превью', blank=False)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название курса")
+    description = models.TextField(verbose_name="Описание курса")
+    preview = models.ImageField(upload_to='lesson/', verbose_name='Превью', blank=False)
+    video_link = models.URLField(verbose_name='Ссылка на видео')
+
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'apps.courses',
     'apps.users',
 ]
@@ -123,7 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
