71b0b303ad2d2ebc1840a9eac01db5c83a7db0da
Great <great.goodwin.92@gmail.com>
2023-08-06 15:04:11 +0300

    HW 24.1

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -6,6 +6,13 @@ class Course(models.Model):
     description = models.TextField(verbose_name="Описание курса")
     preview = models.ImageField(upload_to='course/', verbose_name='Превью', blank=False)
 
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
 
 class Lesson(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название курса")
@@ -13,3 +20,9 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lesson/', verbose_name='Превью', blank=False)
     video_link = models.URLField(verbose_name='Ссылка на видео')
 
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE)
+    city = models.CharField(max_length=235, verbose_name='Город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватарка', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
